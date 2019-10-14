2f10ba21d9489793bbf312fd99e3936d36845a95
罗承扬 <luocy16@mails.tsinghua.edu.cn>
2019-10-14 16:59:57 +0800

    内置的接口Function,相当于Python的apply

--- src/LambdaLearning/InternalFunc.java
+++ src/LambdaLearning/InternalFunc.java
@@ -12,11 +12,12 @@ package LambdaLearning;
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
 
 
-public class InternalFunc {
+public class InternalFunc <T> {
 
     // 对于整数型数组的处理方法
     public static List<Integer> change(Function<Integer, Integer> dosth, List<Integer> a) {
@@ -27,10 +28,22 @@ public class InternalFunc {
             return ans;
     }
 
-    // 可以进一步扩充
+    // 可以进一步扩充到对于更加一般的数组, 实际上利用接口+泛型可以有java中的泛型，只是说我们只需要指定一个Lambda表达式即可
+    public List<T> general_change (Function<T, T> dosth, List<T> a) {
+        List<T> ans = new ArrayList<>();
+        for (T elem : a) {
+            ans.add(dosth.apply(elem));
+        }
+        return ans;
+    }
 
     public static void main (String [] args) {
         Function<Integer, Integer> Double = (Integer a) -> { return a * 2;};
-
+        Function<String, String> DoubleString = (String a) -> { return a + a;};
+        List<Integer> ans = new InternalFunc<Integer>().general_change(Double, new ArrayList<Integer>(Arrays.asList(1, 32,321,32,2)));
+        System.out.println(ans.toString());
+        List<String> ans_string = new InternalFunc<String>().general_change(DoubleString,
+                new ArrayList<String>(Arrays.asList("ada","few","fewe","fewfe\"wfe")));
+        System.out.println(ans_string.toString());
     }
 }
